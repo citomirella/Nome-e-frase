e3db3ec9575166bcb0cea7d33dfa9ed5d80a56be
citomirella <150628385+citomirella@users.noreply.github.com>
2023-11-13 17:55:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Exercicio 1
+
+# declaração  de variáveis
+nome = input("Digite seu nome: ")
+print("Olá, ", nome, ". Bem vindo ao mundo da programação em Python")
+
+# Exercicio 2
+
+seu_nome = input("Digite seu nome: ")
+idade_amigo = input("Digite a idade do seu melhor amigo: ")
+
+mensagem = "Olá, " + seu_nome + "! Seu melhor amigo tem " + idade_amigo + " anos de idade."
+
+print(mensagem)
